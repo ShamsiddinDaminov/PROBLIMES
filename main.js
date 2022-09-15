7386976a4e5951711ac4abf79dfd77a880f96e73
ShamsiddinDaminov <weddasturchi928@gmail.com>
2022-09-15 09:57:16 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,466 @@
+
+// A butun son berilgan. Jumlani rostlikka tekshiring: A son musbat
+
+// let a = 3;
+// if ( a % 3){
+//     console.log("true");
+// }else{
+//     console.log("false");
+// }
+
+// A butun son berilgan. Jumlani rostlikka tekshiring: A toq son
+
+// let a = 3;
+// if(a % 0){
+//     console.log("bu toq son");
+// }
+
+// else{
+//     console.log("bu juft son");
+// }
+
+
+// A butun son berilgan. Junlani rostlikka tekshiring: A juft son
+
+// let a = 5
+// if (a % 4) {
+//     console.log("toq");
+// }else {
+//     console.log("juft");
+// }
+
+// 4 -Ikkita butun son berilgan. Jumlani rostlikka tekshiring. M-s a > 0 && b > 0
+
+
+// let a = 2;
+// let b = 2;
+
+// if(a > 0 && b > 0){
+//     console.log("true");
+// }else {
+//     console.log("false");
+// }
+
+
+// 5 - 3 ta son berilgan a,b,c. a bdan katta b c dan katta rostlikka tekshiring
+
+// let a = 2
+// let b = 4
+// let c = 6
+
+// if (a < 2 && b < 4 && c < 6 ) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// 6 - a, b, c, son berilgan. B son a va c sonlarni orasida yotadi.
+
+// let a = 2;
+// let b = 4;
+// let c = 5;
+
+// if (a < 2 && b < 4 && c < 5) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// 7 - a, b sonlar berilgan sonlarni toq songa tekshiring.
+
+// let a = 2;
+// let b = 2;
+// if (a < 2 && b < 2) {
+//     console.log("toq");
+// }else{
+//     console.log("juft");
+// }
+
+// 8 - a,b,c,d sonlar berilgan barcha musbat
+
+// let a = 2;
+// let b = 4;
+// let c = -5;
+// let d = 4;
+
+// if (
+//     (a > 0 && b > 0 && c > 0 && d > 0)
+ 
+// ){
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+
+// 9 - a,b,c sonlar berilgan kamida ikkitasi musbat berilgan
+
+
+// let a = 2;
+// let b = 2;
+// let c = 2;
+
+// if  (
+//     (a >= 0 && b >= 0 && c < 0) ||
+//     (a >= 0 && b < 0 && c <= 0) ||
+//     (a < 0 && b >= 0 && c >= 0)
+// ){
+//     console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
+
+// 10 -hafta kunlari berilgan raqamga qarab hafta kunlarini nomini noming
+
+// switch (new Date().getDay()) {
+//     case 3:
+//       day = "Sunday";
+//       break;
+//     case 1:
+//       day = "Monday";
+//       break;
+//     case 2:
+//        day = "Tuesday";
+//       break;
+//     case 3:
+//       day = "Wednesday";
+//       break;
+//     case 4:
+//       day = "Thursday";
+//       break;
+//     case 5:
+//       day = "Friday";
+//       break;
+//     case 6:
+//       day = "Saturday";
+//   }
+
+
+// 11 - Telefon nomer kodi orqali qaysi kompaniya tekshiruvi
+
+// let nomer = 98;
+
+// if (nomer == 93){
+//     console.log("usel");
+// }else if (nomer == 97){
+//     console.log("Ums");
+// }else if  (nomer == 99){
+//     console.log("uzmobile");
+// }else if (nomer == 90){
+//     console.log("Beline");
+// }
+// else{
+//     console.log("bunaqa kampaniya mavjud emas");
+// }
+
+// 12 - Butun son berilgan agar son musbat bo'lsa unda sonni bittaga oshiring aks xolda 1taga kamaytiring
+
+
+// let num = 0;
+
+// if (num > 2){
+//     num--
+//     console.log(num);
+// }
+
+// else{ 
+//     num++
+//     console.log(num);
+// }
+
+
+// 13 - Butun son berilgan agar son musbat bo'lsa sonni 3 ga oshiring manfiy bo'lsa unda 2ga kamaytiring
+
+
+// let num = 5;
+
+// if (num > 3){
+//     num ++
+//     console.log(num);
+// }else{
+//     num --
+//     console.log(num);
+// }
+
+
+
+// 14 - Ikkita butun son berilgan kattasin aniqlovchi dastur tuzing
+
+
+// let a = 22;
+// let b = 23;
+// if(a>b){
+//     console.log("a kotta");
+// }else if (b>a){
+//     console.log("b kotta");
+// }
+// else{
+//     console.log("ular teng emas");
+// }
+
+// 15 -3 ta butun son berilgan eng kattasini aniqlovchi dastur tuzing
+
+// let  a = 12;
+// let b = 19;
+// let c = 21;
+
+// if (a>b && a>c){
+//     console.log("a kotta");
+// }else if(b>a && b>c){
+//     console.log("b kotta ");
+// }else  if (c>a && c>b){
+//     console.log("c kotta");
+// }else{
+//     console.log("ular teng emas");
+// }
+
+// 18 - Bahoga qarab stependiyani aniqlovchi dastur tuzing
+
+// let student = 2
+// if( student == 5){
+//     console.log("500");
+// }else if (student == 4){
+//     console.log("400");
+// }else if (student == 3){
+//     console.log("300");
+// }else{
+//     console.log("sizda stependiya chiqmadi");
+// }
+
+
+// 19Butun son berilgan shu songa qarab toq musbat, toq manfiy, juft musbat, juft manfiy deb chiqaruvchi dastur tuzing
+
+// let a = 2;
+ 
+// if (a > 3 && a < 3 && a > 3 && a < 3){
+//     console.log("true");
+// }else {
+//     console.log("false");
+// }
+
+// 20 - Yilga qarab yoshin aniqlovchi dastur tuzing
+
+// let yosh = 2005
+
+// if (yosh == 2000 ) {
+//     console.log("22");
+// } else if (yosh == 2001){
+//     console.log("21");
+// }else if (yosh == 2002) {
+//     console.log("20");
+// }else if (yosh == 2003) {
+//     console.log("19");
+// }else if (yosh == 2004) {
+//     console.log("18");
+// }else if (yosh == 2005) {
+//     console.log("17");
+// }else if (yosh == 2006) {
+//     console.log("16");
+// }else if (yosh == 2007) {
+//     console.log("15");
+// }else {
+//     console.log("bunaqa yosh yuq");
+// }
+
+
+
+// 21 - 3 ta butun son berilgan jumlani rostlikkar tekshiring. A <= B <= C
+
+// let A = 2;
+// let B = 2;
+// let C = 2;
+
+// if (A <=2 && B <= 2 && C <= 2) {
+//     console.log("true");
+// }else {
+//     console.log("false");
+// }
+
+// 22 - ikki butun son berilgan ikki sondan faqat 1tasi toq son hisoblanadi
+
+// let a = 4;
+// let b = 4;
+// if ( a > 0 && b > 0 ){
+//     console.log("toq son");
+// }else{
+//     console.log("juft son");
+// }
+
+// 23 - a,b,c sonlar berilgan har biri musbat son
+
+
+// let a = 4;
+// let b = 5;
+// let c = 6;
+
+// if (a > 0 && b > 0 && c > 0) {
+//     console.log("musbat");
+// } else {
+//     console.log("manfiy");
+// }
+ 
+// 24 - a,b,c sonlardan biri musbat son
+
+// let a = 4;
+// let b = 7;
+// let c = 8;
+
+// if (a > 2 && b >! 7 && c >! 8) {
+//     console.log("musbat");
+// }else{
+//     console.log("manfiy");
+// }
+
+//  25 -  3 xonali son berilgan har bir raqamlari har xil
+
+
+// 26 - 3 xonali son berilgan raqamlari ketma ket o'sib boruvchi joylashgan
+
+// let a = 990;
+
+// let b = a.toString();
+// let c = b.split("");
+// let n = c.sart((a,b) => a - b)
+
+// console.log("n");
+// 27 - Kompyuter nomiga qarab kompyuter summasini chiqaruvchi dastur yarating
+
+// let kampyuter = "Del";
+
+// if (kampyuter == "hp") {
+//     console.log("5.000");
+// }else if (kampyuter == "aser") {
+//     console.log("10.000");
+// }else if (kampyuter == "lenova"){
+//     console.log("11.000");
+// }else if (kampyuter == "Apple-Macbook"){
+//     console.log("10.399000");
+// }else if (kampyuter == "Del"){
+//     console.log("8.000");
+// }else if (kampyuter == "Asus"){
+//     console.log("10.000");
+// }else if (kampyuter == "Honor"){
+//     console.log("10.000");
+// }else if (kampyuter == "MSI"){
+//     console.log("12.000");
+// }else if (kampyuter == "Huawei"){
+//     console.log("10.000");
+// }
+
+
+
+// DATA TYPES - MASHQALAR
+
+
+// FOR- MASHQLAR
+
+
+
+// switch case MASHQLAR
+
+// 1 - 1-7 gacha butun son berilgan kiritilgan songa mos ravishda hafta kunini so'zlarda ifodolovchi dastur tuzing
+
+// const xafta = "4";
+
+// switch (xafta){
+//     case '1':
+//     console.log('dushanba');
+//     break;
+//     case '2':
+//     console.log('seshanba');
+//     break;
+//     case '3':
+//     console.log('chorshanba');
+//     break;
+//     case '4':
+//         console.log('payshanba');
+//         break;
+//         case '5':
+//             console.log('juma');
+//             break;
+//             case '6':
+//                 console.log('shanba');
+//                 break;
+//                 case '7':
+//                     console.log('yakshanba');
+//                     break;
+//                     default :
+//                     console.log(`kechirasz bunaqa xafta yuq`);
+// }
+
+// 2 - K butun son berilgan Baho natijalarini chiqaruvchi dastur tuzing. 1-yomon, 2-qoniqarsiz, 3-qoniqarli, 4-yaxshi, 5 - alo
+
+// const Baho = "6";
+
+// switch (Baho){
+//     case '5':
+//         console.log('Alo');
+//         break;
+//         case '4' :
+//             console.log('yaxshi');
+//             break;
+//             case '3':
+//                 console.log('qoniqarli');
+//                 break;
+//                 case '2':
+//                     console.log('qoniqarsiz');
+//                     break;
+//                     case '1':
+//                         console.log('yomon');
+//                         break;
+//                         default:
+//                             console.log(`kechirasz bunaqa baho yuq `);
+// }
+
+// 3 - Oy raqami berilgan Kiritilgan oy qaysi faslga tegishli ekanin chiqaruvchi dastur tuzing. M - s 2 - qish
+
+// const raqam = "13";
+ 
+// switch  (raqam) {
+//     case '3':
+//         case '4':
+//             case '5':
+//                 console.log('Bahor');
+//                 break;
+//                 case '6':
+//                     case '7':
+//                         case '8':
+//                             console.log('yoz');
+//                             break;
+//                             case '9':
+//                                 case '10':
+//                                     case '11':
+//                                         console.log('kuz');
+//                                         break;
+//                                         case '12':
+//                                             case '1':
+//                                                 case '2':
+//                                                     console.log('qish');
+
+//                                                     break;
+//                                                     default:
+//                                                         console.log(`kechrasz bunaqa fasil yuq `);
+// }
+
+
+//     4 - Oy raqami berilgan shu oyda nechta kun borligin aniqlovchi dastur tuzing
+
+// 6 - Bahoga qarab stependiyani aniqlovchi dastur tuzing
+
+// const stependiya = "4";
+//  switch (stependiya ){
+//     case '5':
+//         console.log("500 ming");
+//         break;
+//         case '4':
+//             console.log("400 ming");
+//             break;
+//             case '3':
+//                 console.log("300 ming");
+//                 break;
+//                 case '2':
+//                     console.log("200 ming");
+//                    default:
+//                     console.log("bunaqa stependiya yuq");
+//  }
